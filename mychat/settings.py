c1918defe13dcb9fce3920cc5ccfaeae333946a3
Abdul Rahman <110932245+Made-me-like-this@users.noreply.github.com>
2025-01-12 14:14:55 +0300

    Update settings.py

--- mychat/settings.py
+++ mychat/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-&pbls0s4p&q#h1z&9_ww4p*+l-pn)@7hzbq)4+(n(f6^d@$-h&
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'matches-separation-climbing-sq.trycloudflare.com']
+ALLOWED_HOSTS = ['talktome-lybb.onrender.com/']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
