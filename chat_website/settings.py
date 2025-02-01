532b2dac57a9319dac1a1faa445496a82d696178
Abdul Rahman <110932245+Made-me-like-this@users.noreply.github.com>
2025-02-01 19:00:22 +0300

    Update settings.py

--- chat_website/settings.py
+++ chat_website/settings.py
@@ -95,14 +95,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     BASE_DIR / 'chat' / 'static',
 ]
 
 # Media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
